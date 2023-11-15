941d5b4424db17c93dd3e70dd7cd967257425503
Belen <belen.pulido@uc3m.es>
2023-11-15 18:21:47 +0100

    adding details

--- R/index_def.R
+++ R/index_def.R
@@ -1,3 +1,17 @@
+#' Epigraph Index (EI) for a univariate functional dataset.
+#'
+#' The Epigraph Index of a curve x is one minus the proportion of curves in the sample
+#' that are above x.
+#'
+#' @param curves A matrix where each row represents a curve, and each column
+#' represents values along the curve.
+#'
+#' @return Return a numeric vector containing the EI for each curve
+#' @export
+#'
+#' @examples
+#' x <- matrix(c(1,2,3,3,2,1,5,2,3,9,8,7),ncol = 3, nrow = 4)
+#' EI(x)
 EI <- function(curves){
   n_curves <- dim(curves)[1]
   lengthcurves <- dim(curves)[2]
